c40e49efbfe4b4190473a5129b3e9117b582e91f
PBernalPolo <pablo.bernal.polo@gmail.com>
2024-07-18 23:16:43 +0200

    feat: add AdditiveMagmaElement and MultiplicativeMagmaElement interfaces

---
+++ src/algebraicStructures/AdditiveMagmaElement.java
@@ -0,0 +1,62 @@
+package algebraicStructures;
+
+
+
+/**
+ * {@link AdditiveMagmaElement} represents elements of a magma with addition as the magma operation.
+ * 
+ * @param <T>   concrete type of {@link AdditiveMagmaElement}. We use CRTP to bound the type to interfaces that extend this interface.
+ * 
+ * @see <a href>https://en.wikipedia.org/wiki/Magma_(algebra)</a>
+ */
+public interface AdditiveMagmaElement<T extends AdditiveMagmaElement<T>>
+    extends
+        SetElement<T>
+{
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC ABSTRACT METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    /**
+     * Adds {@code other} to {@code this}.
+     * <p>
+     * Result is returned as a new instance.
+     * 
+     * @param other     {@code other} object to be added to {@code this}.
+     * @return  sum of {@code this} and {@code other} stored in a new instance.
+     */
+    public T add( T other );
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC DEFAULT METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    /**
+     * Adds in-place {@code other} to {@code this}.
+     * <p>
+     * Operation done in-place.
+     * 
+     * @param other     {@code other} object to be added to {@code this}.
+     * @return  sum of {@code this} and {@code other} stored in {@code this}.
+     */
+    default T addInplace( T other )
+    {
+        return this.setTo( this.add( other ) );
+    }
+    
+
+    /**
+     * Adds {@code first} and {@code second} and stores the result in {@code this}.
+     * 
+     * @param first     first term of the addition.
+     * @param second    second term of the addition.
+     * @return  sum of {@code first} and {@code second} stored in {@code this}.
+     */
+    default T setToSum( T first , T second )
+    {
+        return this.setTo( first.add( second ) );
+    }
+    
+}
---
+++ src/algebraicStructures/MultiplicativeMagmaElement.java
@@ -0,0 +1,62 @@
+package algebraicStructures;
+
+
+
+/**
+ * {@link MultiplicativeMagmaElement} represents elements of a magma with multiplication as the magma operation.
+ * 
+ * @param <T>   concrete type of {@link MultiplicativeMagmaElement}. We use CRTP to bound the type to interfaces that extend this interface.
+ * 
+ * @see <a href>https://en.wikipedia.org/wiki/Magma_(algebra)</a>
+ */
+public interface MultiplicativeMagmaElement<T extends MultiplicativeMagmaElement<T>>
+	extends
+        SetElement<T>
+{
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC ABSTRACT METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    /**
+     * Multiplies {@code this} times {@code other}.
+     * <p>
+     * Result is returned as a new instance.
+     * 
+     * @param other     multiplier: {@code other} object to be multiplied by {@code this}.
+     * @return  product of {@code this} times {@code other} stored in a new instance.
+     */
+    T multiply( T other );
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC DEFAULT METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    /**
+     * Multiplies in-place {@code this} times {@code other}.
+     * <p>
+     * Operation done in-place.
+     * 
+     * @param other     multiplier: {@code other} object to be multiplied by {@code this}.
+     * @return  product of {@code this} times {@code other} stored in {@code this}.
+     */
+    default T multiplyInplace( T other )
+    {
+        return this.setTo( this.multiply( other ) );
+    }
+    
+    
+    /**
+     * Multiplies {@code first} and {@code second} and stores the result in {@code this}.
+     * 
+     * @param first     first factor of the multiplication.
+     * @param second    second factor of the multiplication.
+     * @return  product of the multiplication stored in {@code this}.
+     */
+    default T setToProduct( T first , T second )
+    {
+        return this.setTo( first.multiply( second ) );
+    }
+    
+}
