b35fb700384e02a0abd7cb89651d0aae7387fffa
PBernalPolo <pablo.bernal.polo@gmail.com>
2024-07-19 19:03:08 +0200

    feat: add 2d and 3d vector implementations

---
+++ src/types/Vector2.java
@@ -0,0 +1,276 @@
+package types;
+
+
+import java.util.Random;
+
+import algebraicStructures.AdditiveAbelianGroupElement;
+import algebraicStructures.MetricSpaceElement;
+import algebraicStructures.VectorSpaceElement;
+
+
+
+/**
+ * Implement 2d vectors.
+ */
+public class Vector2
+    implements
+        AdditiveAbelianGroupElement<Vector2>,
+        VectorSpaceElement<Vector2>,
+        MetricSpaceElement<Vector2>
+{
+    ////////////////////////////////////////////////////////////////
+    // PRIVATE VARIABLES
+    ////////////////////////////////////////////////////////////////
+    
+    /**
+     * First component of the 2d vector.
+     */
+    private double vx;
+    
+    /**
+     * Second component of the 2d vector.
+     */
+    private double vy;
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC CONSTRUCTORS
+    ////////////////////////////////////////////////////////////////
+    
+    /**
+     * Constructs a 2d vector.
+     * 
+     * @param x     first component of the 2d vector.
+     * @param y     second component of the 2d vector.
+     */
+    public Vector2( double x , double y )
+    {
+        this.vx = x;
+        this.vy = y;
+    }
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    public void setX( double x )
+    {
+        this.vx = x;
+    }
+    
+    
+    public void setY( double y )
+    {
+        this.vy = y;
+    }
+    
+    
+    public double x()
+    {
+        return vx;
+    }
+    
+    
+    public double y()
+    {
+        return vy;
+    }
+    
+    
+    public String toString()
+    {
+        return String.format( "( %2.16f , %2.16f )" , this.x() , this.y() );
+    }
+    
+    
+    public Vector2 print()
+    {
+        System.out.println( this.toString() );
+        return this;
+    }
+    
+    
+    public Vector2 copy()
+    {
+        return new Vector2( this.x() , this.y() );
+    }
+    
+    
+    public Vector2 setTo( Vector2 other )
+    {
+        this.vx = other.x();
+        this.vy = other.y();
+        return this;
+    }
+    
+    
+    public boolean equals( Vector2 other )
+    {
+        return (  this.x() == other.x()  &&
+                  this.y() == other.y()  );
+    }
+    
+    
+    public boolean equalsApproximately( Vector2 other , double tolerance )
+    {
+        return ( this.distanceFrom( other ) < tolerance );
+    }
+    
+    
+    public Vector2 add( Vector2 other )
+    {
+        return new Vector2( this.x() + other.x() ,
+                                this.y() + other.y() );
+    }
+
+
+    public Vector2 addInplace( Vector2 other )
+    {
+        this.vx += other.x();
+        this.vy += other.y();
+        return this;
+    }
+    
+    
+    public Vector2 identityAdditive()
+    {
+        return Vector2.zero();
+    }
+
+    
+    public Vector2 inverseAdditive()
+    {
+        return new Vector2( -this.x() , -this.y() );
+    }
+
+
+    public Vector2 inverseAdditiveInplace()
+    {
+        this.vx = -this.x();
+        this.vy = -this.y();
+        return this;
+    }
+
+
+    public Vector2 subtract( Vector2 other )
+    {
+        return new Vector2( this.x() - other.x() ,
+                                this.y() - other.y() );
+    }
+
+
+    public Vector2 subtractInplace( Vector2 other )
+    {
+        this.vx -= other.x();
+        this.vy -= other.y();
+        return this;
+    }
+    
+    
+    public Vector2 scale( double scalar )
+    {
+        return new Vector2( this.x() * scalar ,
+                                this.y() * scalar );
+    }
+
+
+    public Vector2 scaleInplace( double scalar )
+    {
+        this.vx *= scalar;
+        this.vy *= scalar;
+        return this;
+    }
+    
+    
+    public double dot( Vector2 other )
+    {
+        return ( this.x() * other.x() +
+                 this.y() * other.y() );
+    }
+    
+    
+    public double normSquared()
+    {
+        return this.dot( this );
+    }
+    
+    
+    public double norm()
+    {
+        return Math.sqrt( this.normSquared() );
+    }
+
+
+    public Vector2 normalize()
+    {
+        return this.scale( 1.0/this.norm() );
+    }
+
+
+    public Vector2 normalizeInplace()
+    {
+        return this.scaleInplace( 1.0/this.norm() );
+    }
+
+
+
+    public double distanceFrom( Vector2 other )
+    {
+        return this.subtract( other ).norm();
+    }
+    
+    
+    public RealMatrix outerProduct( Vector2 other )
+    {
+        return RealMatrix.matrix2x2( this.x() * other.x() , this.x() * other.y() ,
+                                     this.y() * other.x() , this.y() * other.y() );
+    }
+    
+    
+    public RealMatrix toRealMatrixAsColumn()
+    {
+        return RealMatrix.fromRealVector2AsColumn( this );
+    }
+    
+    
+    public RealMatrix toRealMatrixAsRow()
+    {
+        return RealMatrix.fromRealVector2AsRow( this );
+    }
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC STATIC METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    public static Vector2 zero()
+    {
+        return new Vector2( 0.0 , 0.0 );
+    }
+    
+    
+    public static Vector2 i()
+    {
+        return new Vector2( 1.0 , 0.0 );
+    }
+
+
+    public static Vector2 j()
+    {
+        return new Vector2( 0.0 , 1.0 );
+    }
+
+
+    public static Vector2 random( Random randomNumberGenerator )
+    {
+        return new Vector2(
+                randomNumberGenerator.nextGaussian() ,
+                randomNumberGenerator.nextGaussian() );
+    }
+    
+}
+
---
+++ src/types/Vector3.java
@@ -0,0 +1,322 @@
+package types;
+
+import java.util.Random;
+
+import algebraicStructures.AdditiveAbelianGroupElement;
+import algebraicStructures.MetricSpaceElement;
+import algebraicStructures.VectorSpaceElement;
+
+
+
+public class Vector3
+    implements
+        AdditiveAbelianGroupElement<Vector3>,
+        VectorSpaceElement<Vector3>,
+        MetricSpaceElement<Vector3>
+{
+    ////////////////////////////////////////////////////////////////
+    // PRIVATE VARIABLES
+    ////////////////////////////////////////////////////////////////
+    private double vx;
+    private double vy;
+    private double vz;
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC CONSTRUCTORS
+    ////////////////////////////////////////////////////////////////
+    
+    public Vector3( double x , double y , double z )
+    {
+        this.vx = x;
+        this.vy = y;
+        this.vz = z;
+    }
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    public void setX( double x )
+    {
+        this.vx = x;
+    }
+    
+    
+    public void setY( double y )
+    {
+        this.vy = y;
+    }
+    
+    
+    public void setZ( double z )
+    {
+        this.vz = z;
+    }
+    
+    
+    public double x()
+    {
+        return vx;
+    }
+    
+    
+    public double y()
+    {
+        return vy;
+    }
+    
+    
+    public double z()
+    {
+        return vz;
+    }
+    
+    
+    public String toString()
+    {
+        return String.format( "( %2.16f , %2.16f , %2.16f )" , this.x() , this.y() , this.z() );
+    }
+    
+    
+    public Vector3 print()
+    {
+        System.out.println( this.toString() );
+        return this;
+    }
+    
+    
+    public Vector3 copy()
+    {
+        return new Vector3( this.x() , this.y() , this.z() );
+    }
+    
+    
+    public Vector3 setTo( Vector3 other )
+    {
+        this.vx = other.x();
+        this.vy = other.y();
+        this.vz = other.z();
+        return this;
+    }
+    
+    
+    public Vector3 setToZero()
+    {
+        this.vx = 0.0;
+        this.vy = 0.0;
+        this.vz = 0.0;
+        return this;
+    }
+    
+    
+    public boolean equals( Vector3 other )
+    {
+        return (  this.x() == other.x()  &&
+                  this.y() == other.y()  &&
+                  this.z() == other.z()  );
+    }
+    
+    
+    public boolean equalsApproximately( Vector3 other , double tolerance )
+    {
+        return ( this.distanceFrom( other ) < tolerance );
+    }
+    
+    
+    public Vector3 add( Vector3 other )
+    {
+        return new Vector3( this.x() + other.x() ,
+                                this.y() + other.y() ,
+                                this.z() + other.z() );
+    }
+
+
+    public Vector3 addInplace( Vector3 other )
+    {
+        this.vx += other.x();
+        this.vy += other.y();
+        this.vz += other.z();
+        return this;
+    }
+    
+    
+    public Vector3 identityAdditive()
+    {
+        return Vector3.zero();
+    }
+
+    
+    public Vector3 inverseAdditive()
+    {
+        return new Vector3( -this.x() , -this.y() , -this.z() );
+    }
+
+
+    public Vector3 inverseAdditiveInplace()
+    {
+        this.vx = -this.x();
+        this.vy = -this.y();
+        this.vz = -this.z();
+        return this;
+    }
+
+
+    public Vector3 subtract( Vector3 other )
+    {
+        return new Vector3( this.x() - other.x() ,
+                                this.y() - other.y() ,
+                                this.z() - other.z() );
+    }
+
+
+    public Vector3 subtractInplace( Vector3 other )
+    {
+        this.vx -= other.x();
+        this.vy -= other.y();
+        this.vz -= other.z();
+        return this;
+    }
+    
+    
+    public Vector3 scale( double scalar )
+    {
+        return new Vector3( this.x() * scalar ,
+                                this.y() * scalar ,
+                                this.z() * scalar );
+    }
+
+
+    public Vector3 scaleInplace( double scalar )
+    {
+        this.vx *= scalar;
+        this.vy *= scalar;
+        this.vz *= scalar;
+        return this;
+    }
+    
+    
+    public double dot( Vector3 other )
+    {
+        return ( this.x() * other.x() +
+                 this.y() * other.y() +
+                 this.z() * other.z() );
+    }
+    
+    
+    public double normSquared()
+    {
+        return this.dot( this );
+    }
+    
+    
+    public double norm()
+    {
+        return Math.sqrt( this.normSquared() );
+    }
+
+
+    public Vector3 normalize()
+    {
+        return this.scale( 1.0/this.norm() );
+    }
+
+
+    public Vector3 normalizeInplace()
+    {
+        return this.scaleInplace( 1.0/this.norm() );
+    }
+
+
+
+    public double distanceFrom( Vector3 other )
+    {
+        return this.subtract( other ).norm();
+    }
+    
+    
+    public double angleFrom( Vector3 other )
+    {
+        double dot = this.normalize().dot( other.normalize() );
+        return ( ( dot < -1.0 )? Math.PI : ( ( 1.0 < dot )? 0.0 : Math.acos( dot ) ) );
+    }
+    
+    
+    public Vector3 crossProduct( Vector3 other )
+    {
+        return new Vector3( this.y() * other.z() - this.z() * other.y() ,
+                                this.z() * other.x() - this.x() * other.z() ,
+                                this.x() * other.y() - this.y() * other.x() );
+    }
+    
+    
+    public RealMatrix crossProductMatrix()
+    {
+        return RealMatrix.matrix3x3( 0.0      , -this.z() ,  this.y() ,
+                                     this.z() ,  0.0      , -this.x() ,
+                                    -this.y() ,  this.x() ,  0.0      );
+    }
+    
+    
+    public RealMatrix outerProduct( Vector3 other )
+    {
+        return RealMatrix.matrix3x3( this.x() * other.x() , this.x() * other.y() , this.x() * other.z() ,
+                                     this.y() * other.x() , this.y() * other.y() , this.y() * other.z() ,
+                                     this.z() * other.x() , this.z() * other.y() , this.z() * other.z() );
+    }
+    
+    
+    public RealMatrix toRealMatrixAsColumn()
+    {
+        return RealMatrix.fromRealVector3AsColumn( this );
+    }
+    
+    
+    public RealMatrix toRealMatrixAsRow()
+    {
+        return RealMatrix.fromRealVector3AsRow( this );
+    }
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC STATIC METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    public static Vector3 zero()
+    {
+        return new Vector3( 0.0 , 0.0 , 0.0 );
+    }
+    
+    
+    public static Vector3 i()
+    {
+        return new Vector3( 1.0 , 0.0 , 0.0 );
+    }
+
+
+    public static Vector3 j()
+    {
+        return new Vector3( 0.0 , 1.0 , 0.0 );
+    }
+
+
+    public static Vector3 k()
+    {
+        return new Vector3( 0.0 , 0.0 , 1.0 );
+    }
+    
+    
+    public static Vector3 random( Random randomNumberGenerator )
+    {
+        return new Vector3(
+                randomNumberGenerator.nextGaussian() ,
+                randomNumberGenerator.nextGaussian() ,
+                randomNumberGenerator.nextGaussian() );
+    }
+    
+}
+
