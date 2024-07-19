d25299e3df93ac8eec67d5feebd83889e804ae20
PBernalPolo <pablo.bernal.polo@gmail.com>
2024-07-19 19:06:24 +0200

    test: add tests for Quaternion

---
+++ tests/types/QuaternionTest.java
@@ -0,0 +1,54 @@
+package types;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import algebraicStructures.AdditiveAbelianGroupElementTester;
+import algebraicStructures.MetricSpaceElementTester;
+import algebraicStructures.MultiplicativeGroupElementTester;
+import algebraicStructures.VectorSpaceElementTester;
+
+
+
+/**
+ * Implements test methods for {@link Quaternion}.
+ */
+class QuaternionTest
+    implements
+        AdditiveAbelianGroupElementTester<Quaternion>,
+        MultiplicativeGroupElementTester<Quaternion>,
+        VectorSpaceElementTester<Quaternion>,
+        MetricSpaceElementTester<Quaternion>
+{
+    ////////////////////////////////////////////////////////////////
+    // PUBLIC METHODS
+    ////////////////////////////////////////////////////////////////
+    
+    /**
+     * {@inheritDoc}
+     */
+    public List<Quaternion> getElementList()
+    {
+        List<Quaternion> output = new ArrayList<Quaternion>();
+        output.add( Quaternion.zero() );
+        output.add( Quaternion.zero() );
+        output.add( Quaternion.one() );
+        output.add( Quaternion.one() );
+        output.add( Quaternion.i() );
+        output.add( Quaternion.i() );
+        output.add( Quaternion.j() );
+        output.add( Quaternion.j() );
+        output.add( Quaternion.k() );
+        output.add( Quaternion.k() );
+        Random rng = new Random( 42 );
+        for( int i=0; i<1000; i++ ) {
+            Quaternion r = Quaternion.random( rng );
+            output.add( r );
+            output.add( r.copy() );
+        }
+        return output;
+    }
+    
+}
