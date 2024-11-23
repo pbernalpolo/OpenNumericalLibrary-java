3505eed8f3d77451f2453772fb92466eef72673b
PBernalPolo <pablo.bernal.polo@gmail.com>
2024-11-23 20:02:42 +0100

    refactor: adapt GaussianFit example to changes

--- src/examples/java/optimization/GaussianFit.java
+++ src/examples/java/optimization/GaussianFit.java
@@ -4,12 +4,10 @@ package optimization;
 import java.util.ArrayList;
 import java.util.List;
 
-import numericalLibrary.optimization.LevenbergMarquardtAlgorithm;
-import numericalLibrary.optimization.robustFunctions.WelschRobustFunction;
-import numericalLibrary.optimization.LeastSquaresDataPair;
-import numericalLibrary.optimization.LeastSquaresFunction;
-import numericalLibrary.optimization.stoppingCriteria.IterationThresholdStoppingCriterion;
-import numericalLibrary.optimization.stoppingCriteria.MaximumIterationsWithoutImprovementStoppingCriterion;
+import numericalLibrary.optimization.algorithms.GradientDescentAlgorithm;
+import numericalLibrary.optimization.algorithms.LevenbergMarquardtAlgorithm;
+import numericalLibrary.optimization.lossFunctions.MeanSquaredErrorLQL;
+import numericalLibrary.optimization.lossFunctions.RobustMeanSquaredErrorLQL;
 import numericalLibrary.types.Matrix;
 import numericalLibrary.util.GaussianFunction;
 
@@ -28,46 +26,44 @@ public class GaussianFit
     {
         // Generate a known Gaussian function.
         GaussianFunction gaussianKnown = new GaussianFunction();
-        Matrix parameters = Matrix.fromArrayAsColumn( new double[] { 3.0 , 2.0 , 5.0 } );
+        Matrix parameters = Matrix.fromArrayAsRow( new double[] { 3.0 , 2.0 , 5.0 } );
         gaussianKnown.setParameters( parameters );
         
         // Sample with the known Gaussian function.
-        List<LeastSquaresDataPair<Double>> empiricalPairList = new ArrayList<LeastSquaresDataPair<Double>>();
-        List<Double> inputWeightList = new ArrayList<Double>();
+        List<Double> inputList = new ArrayList<Double>();
+        List<Matrix> targetList = new ArrayList<Matrix>();
         for( int m=0; m<10; m++ )
         {
             double x = m/5.0;
             gaussianKnown.setInput( x );
-            empiricalPairList.add( new LeastSquaresDataPair<Double>( gaussianKnown.getOutput() , x ) );
-            inputWeightList.add( 1.0 );
+            inputList.add( x );
+            targetList.add( gaussianKnown.getOutput() );
         }
         
-        // Find a solution using the Levenberg-Marquardt algorithm.
-        LevenbergMarquardtAlgorithm<LeastSquaresDataPair<Double>> algorithm = new LevenbergMarquardtAlgorithm<LeastSquaresDataPair<Double>>();
-        algorithm.setRobustFunction( new WelschRobustFunction( 1.0 ) );
-        algorithm.setInputList( empiricalPairList , inputWeightList );
+        // Define the loss function.
         GaussianFunction gaussianUnknown = new GaussianFunction();
-        gaussianUnknown.setParameters( Matrix.fromArrayAsColumn( new double[] { 1.0 , 0.0 , 1.0 } ) );
-        algorithm.setOptimizableFunction( new LeastSquaresFunction<Double>( gaussianUnknown ) );
-        //algorithm.setStoppingCriterion( new IterationThresholdStoppingCriterion( 1000 ) );
+        gaussianUnknown.setParameters( Matrix.fromArrayAsRow( new double[] { 1.0 , 0.0 , 1.0 } ) );
+        //MeanSquaredErrorLQL<Double> loss = new MeanSquaredErrorLQL<Double>( gaussianUnknown );
+        RobustMeanSquaredErrorLQL<Double> loss = new RobustMeanSquaredErrorLQL<Double>( gaussianUnknown );
+        loss.setInputListAndTargetList( inputList , targetList );
+        
+        // Find a solution using the Levenberg-Marquardt algorithm.
+        //GradientDescentAlgorithm algorithm = new GradientDescentAlgorithm( loss );
+        LevenbergMarquardtAlgorithm algorithm = new LevenbergMarquardtAlgorithm( loss );
         algorithm.setDampingFactor( 1.0e-4 );
         
         algorithm.initialize();
-        System.out.println( "error after " + algorithm.getIterationLast() + " iterations: " + algorithm.getCostLast() );
+        System.out.println( "error after " + algorithm.getIteration() + " iterations: " + algorithm.getCost() );
         for( int i=0; i<50; i++) {
             algorithm.step();
-            System.out.println( "error after " + algorithm.getIterationLast() + " iterations: " + algorithm.getCostLast() );
+            System.out.println( "error after " + algorithm.getIteration() + " iterations: " + algorithm.getCost() );
         }
         //algorithm.iterate();
         System.out.println();
         
-        System.out.println( "Last solution was obtained after " + algorithm.getIterationLast() + " iterations, and produces an error of " + algorithm.getCostLast() );
+        System.out.println( "Last solution was obtained after " + algorithm.getIteration() + " iterations, and produces an error of " + algorithm.getCost() );
         System.out.println( "Last solution is:" );
-        algorithm.getSolutionLast().print();
-        
-        System.out.println( "Best solution was obtained after " + algorithm.getIterationBest() + " iterations, and produces an error of " + algorithm.getCostBest() );
-        System.out.println( "Best solution is:" );
-        algorithm.getSolutionBest().print();
+        gaussianUnknown.getParameters().print();
     }
     
 }
