9ca246b8778bb8edbffd25da8bd3829a178be3a2
PBernalPolo <pablo.bernal.polo@gmail.com>
2024-11-30 16:58:46 +0100

    feat: add simple example on using different types

---
+++ src/examples/java/numericalLibrary/types/TypesExample.java
@@ -0,0 +1,64 @@
+package numericalLibrary.types;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import numericalLibrary.algebraicStructures.VectorSpaceElement;
+
+
+
+/**
+ * Example that shows how to set up and use the Levenberg-Marquardt algorithm to fit a Gaussian function to empirical data.
+ */
+public class TypesExample
+{
+    ////////////////////////////////////////////////////////////////
+    // ENTRY POINT
+    ////////////////////////////////////////////////////////////////
+    
+    public static void main( String[] args )
+    {
+        // Create a list of real numbers.
+        List<RealNumber> realNumberList = new ArrayList<RealNumber>();
+        for( int i=0; i<7; i++ ) {
+            realNumberList.add( new RealNumber( i ) );
+        }
+        
+        // Create a list of complex numbers.
+        List<ComplexNumber> complexNumberList = new ArrayList<ComplexNumber>();
+        for( int i=0; i<5; i++ ) {
+            complexNumberList.add( ComplexNumber.fromRealPartAndImaginaryPart( i , - 3 * i ) );
+        }
+        
+        // Create a list of quaternions.
+        List<Quaternion> quaternionList = new ArrayList<Quaternion>();
+        for( int i=0; i<3; i++ ) {
+            quaternionList.add( Quaternion.fromScalarAndVectorPart( i , new Vector3( -2 * i , 3 * i , 5 * i ) ) );
+        }
+        
+        // Create a list of matrices.
+        List<Matrix> matrixList = new ArrayList<Matrix>();
+        for( int i=0; i<2; i++ ) {
+            matrixList.add( Matrix.one( i+2 ) );
+        }
+        
+        // Put all elements in a list of VectorSpaceElements.
+        List<VectorSpaceElement<?>> vectorSpaceElementList = new ArrayList<VectorSpaceElement<?>>();
+        vectorSpaceElementList.addAll( realNumberList );
+        vectorSpaceElementList.addAll( complexNumberList );
+        vectorSpaceElementList.addAll( quaternionList );
+        vectorSpaceElementList.addAll( matrixList );
+        
+        // Print VectorSpaceElements before and after scaling.
+        for( VectorSpaceElement<?> x : vectorSpaceElementList ) {
+            System.out.println( "Before it was:\n" + x.toString() + "\n and after scaling:\n" + x.scaleInplace( 2 ).toString() + "\n" );
+        }
+        
+        // Print all VectorSpaceElements before scaling them once again.
+        for( VectorSpaceElement<?> x : vectorSpaceElementList ) {
+            x.print().scaleInplace( 3.0 );
+        }
+    }
+    
+}
