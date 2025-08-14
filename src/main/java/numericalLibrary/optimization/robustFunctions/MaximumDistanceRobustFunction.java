a5121b31de570dec489bce7124f6e91009b3a8e9
PBernalPolo <pablo.bernal.polo@gmail.com>
2025-08-14 16:49:46 +0200

    refactor: robust functions after interface change
    We also improve documentation, and introduce variables to improve
    efficiency.

--- src/main/java/numericalLibrary/optimization/robustFunctions/MaximumDistanceRobustFunction.java
+++ src/main/java/numericalLibrary/optimization/robustFunctions/MaximumDistanceRobustFunction.java
@@ -4,9 +4,15 @@ package numericalLibrary.optimization.robustFunctions;
 
 /**
  * {@link RobustFunction} defined as:
- * f( ||e||^2 ) = ||e||^2   if ||e|| < k
- *              = k^2       if ||e|| >= k
+ * f( x ) = x^2/2	if x < k
+ *        = k^2/2   if x >= k
  * where k is the value after which the function is constant.
+ * The derivative is:
+ * f'( x ) = x	if x < k
+ * 		   = 0 	if x >= k
+ * and its weight function
+ * w( x ) = 1	if x < k
+ * 		  = 0	if x >= k
  * 
  * @see https://arxiv.org/abs/1810.01474
  */
@@ -22,6 +28,11 @@ public class MaximumDistanceRobustFunction
      */
     private double kSquared;
     
+    /**
+     * Square of parameter k after which the function is constant divided by 2.
+     */
+    private double kSquaredOver2;
+    
     
     
     ////////////////////////////////////////////////////////////////
@@ -36,6 +47,7 @@ public class MaximumDistanceRobustFunction
     public MaximumDistanceRobustFunction( double kParameter )
     {
         this.kSquared = kParameter * kParameter;
+        this.kSquaredOver2 = 0.5 * this.kSquared;
     }
     
     
@@ -47,12 +59,12 @@ public class MaximumDistanceRobustFunction
     /**
      * {@inheritDoc}
      */
-    public double f( double xSquared )
+    public double rho( double xSquared )
     {
         if( xSquared < this.kSquared ) {
-            return xSquared;
+            return xSquared/2.0;
         } else {
-            return this.kSquared;
+            return this.kSquaredOver2;
         }
     }
     
@@ -60,7 +72,7 @@ public class MaximumDistanceRobustFunction
     /**
      * {@inheritDoc}
      */
-    public double f1( double xSquared )
+    public double weight( double xSquared )
     {
         if( xSquared < this.kSquared ) {
             return 1.0;
--- src/main/java/numericalLibrary/optimization/robustFunctions/WelschRobustFunction.java
+++ src/main/java/numericalLibrary/optimization/robustFunctions/WelschRobustFunction.java
@@ -4,9 +4,11 @@ package numericalLibrary.optimization.robustFunctions;
 
 /**
  * {@link RobustFunction} defined as:
- * f( ||e||^2 ) = k^2 / 2 ( 1 - exp( -||e||^2 / k^2 ) )
- * and derivative
- * f'( ||e||^2 ) = exp( - ||e||^2 / k^2 ) / 2
+ * f( x ) = k^2 / 2 ( 1 - exp( -x^2 / k^2 ) )
+ * with derivative:
+ * f'( x ) = x exp( - x^2 / k^2 )
+ * and weight function:
+ * w( x ) = exp( -x^2 / k^2 )
  * 
  * @see https://arxiv.org/abs/1810.01474
  */
@@ -22,6 +24,12 @@ public class WelschRobustFunction
      */
     private double kSquared;
     
+    /**
+     * Square of parameter k of the Welsch function divided by 2.
+     */
+    private double kSquaredOver2;
+    
+    
     
     ////////////////////////////////////////////////////////////////
     // PUBLIC CONSTRUCTORS
@@ -35,6 +43,7 @@ public class WelschRobustFunction
     public WelschRobustFunction( double kParameter )
     {
         this.kSquared = kParameter * kParameter;
+        this.kSquaredOver2 = 0.5 * this.kSquared;
     }
     
     
@@ -46,18 +55,18 @@ public class WelschRobustFunction
     /**
      * {@inheritDoc}
      */
-    public double f( double xSquared )
+    public double rho( double xSquared )
     {
-        return this.kSquared / 2.0 * ( 1.0 - Math.exp( - xSquared / this.kSquared ) );
+        return this.kSquaredOver2 * ( 1.0 - Math.exp( - xSquared / this.kSquared ) );
     }
     
     
     /**
      * {@inheritDoc}
      */
-    public double f1( double xSquared )
+    public double weight( double xSquared )
     {
-        return Math.exp( - xSquared / this.kSquared ) / 2.0;
+        return Math.exp( - xSquared / this.kSquared );
     }
     
 }
