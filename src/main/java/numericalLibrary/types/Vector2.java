e07d3961dde91bf333bc4f64f80fef15ce90b52b
PBernalPolo <pablo.bernal.polo@gmail.com>
2025-10-24 10:28:31 +0200

    refactor: make constructors private to use Static Factory Method
    in Quaternion, Vector2, and Vector3

--- src/main/java/numericalLibrary/types/Vector2.java
+++ src/main/java/numericalLibrary/types/Vector2.java
@@ -19,7 +19,7 @@ public class Vector2
         MetricSpaceElement<Vector2>
 {
     ////////////////////////////////////////////////////////////////
-    // PRIVATE VARIABLES
+    /// PRIVATE VARIABLES
     ////////////////////////////////////////////////////////////////
     
     /**
@@ -35,25 +35,7 @@ public class Vector2
     
     
     ////////////////////////////////////////////////////////////////
-    // PUBLIC CONSTRUCTORS
-    ////////////////////////////////////////////////////////////////
-    
-    /**
-     * Constructs a 2d vector.
-     * 
-     * @param x     first component of the 2d vector.
-     * @param y     second component of the 2d vector.
-     */
-    public Vector2( double x , double y )
-    {
-        this.vx = x;
-        this.vy = y;
-    }
-    
-    
-    
-    ////////////////////////////////////////////////////////////////
-    // PUBLIC METHODS
+    /// PUBLIC METHODS
     ////////////////////////////////////////////////////////////////
     
     /**
@@ -326,7 +308,7 @@ public class Vector2
     
     
     ////////////////////////////////////////////////////////////////
-    // PUBLIC STATIC METHODS
+    /// PUBLIC STATIC METHODS
     ////////////////////////////////////////////////////////////////
     
     /**
@@ -369,6 +351,34 @@ public class Vector2
     
     
     /**
+     * Returns a new {@link Vector2} constructed from its components.
+     * 
+     * @param xComponent	x-component of the {@link Vector2}.
+     * @param yComponent	y-component of the {@link Vector2}.
+     * @return	new {@link Vector2} constructed from its components.
+     */
+    public static Vector2 fromComponents( double xComponent , double yComponent )
+    {
+    	return new Vector2( xComponent , yComponent );
+    }
+    
+    
+    /**
+     * Returns a new {@link Vector2} constructed from a flat array.
+     * 
+     * @param flatArray     array that contains the {@link Vector2} elements as {@code double[] { v.x() , v.y() }}.
+     * @return  new {@link Vector2} constructed from a flat array.
+     */
+    public static Vector2 fromFlatArray( double[] flatArray )
+    {
+        if( flatArray.length < 2 ) {
+            throw new IllegalArgumentException( "Not enough elements in flatArray; found " + flatArray.length + "; expected 2 or more." );
+        }
+        return new Vector2( flatArray[0] , flatArray[1] );
+    }
+    
+    
+    /**
      * Returns a new random {@link Vector2} with normally distributed components sampled from {@link Random}.
      * <p>
      * The components of the {@link Vector2} are normally distributed with mean {@code 0.0} and standard deviation {@code 1.0}.
@@ -381,6 +391,24 @@ public class Vector2
         return new Vector2(
                 randomNumberGenerator.nextGaussian() ,
                 randomNumberGenerator.nextGaussian() );
+    }
+    
+    
+    
+    ////////////////////////////////////////////////////////////////
+    /// PRIVATE CONSTRUCTORS
+    ////////////////////////////////////////////////////////////////
+    
+    /**
+     * Constructs a 2d vector.
+     * 
+     * @param x     first component of the 2d vector.
+     * @param y     second component of the 2d vector.
+     */
+    private Vector2( double x , double y )
+    {
+        this.vx = x;
+        this.vy = y;
     }
     
 }
