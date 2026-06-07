a1553b39157ab81d8dca59c2e892b1fb3ddb2222
pbernalpolo <pablo.bernal.polo@gmail.com>
2026-06-07 22:24:54 +0200

    refactor: LegendrePolynomialEvaluator to pre-compute coefficients

--- src/main/java/numericalLibrary/functions/LegendrePolynomialEvaluator.java
+++ src/main/java/numericalLibrary/functions/LegendrePolynomialEvaluator.java
@@ -4,6 +4,9 @@ package numericalLibrary.functions;
 
 public class LegendrePolynomialEvaluator
 {
+	private final double[] an;
+	private final double[] bn;
+	
 	/**
 	 * Result of evaluation of P_n(x).
 	 */
@@ -18,8 +21,15 @@ public class LegendrePolynomialEvaluator
 		if( nMaximum < 2 ) {
 			nMaximum = 2;
 		}
+		this.an = new double[ nMaximum + 1 ];
+		this.bn = new double[ nMaximum + 1 ];
 		this.pn = new double[ nMaximum + 1 ];
 		this.pn[0] = 1.0;
+		for( int n=1; n<this.pn.length; n++ ) {
+			double nPlus1 = n + 1.0;
+			this.an[n] = ( n + nPlus1 ) / nPlus1;
+			this.bn[n] = n / nPlus1;
+		}
 	}
 	
 	
@@ -28,9 +38,8 @@ public class LegendrePolynomialEvaluator
 		this.pn[1] = x;
 		// ( n + 1 ) P_{n+1}(x) = ( 2 n + 1 ) x P_n(x) - n P_{n-1}(x)
 		for(  int n=1, nPlus1=2;  nPlus1<this.pn.length;  n++, nPlus1++  ) {
-			this.pn[nPlus1] = ( ( n + nPlus1 ) * x * this.pn[n] - n * this.pn[n-1] ) / nPlus1;
+			this.pn[nPlus1] = this.an[n] * x * this.pn[n] - this.bn[n] * this.pn[n-1];
 		}
-		
 	}
 	
 	
