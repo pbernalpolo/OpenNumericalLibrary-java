04ea68f1785d24b3e08d11db0193294a460adfcc
pbernalpolo <pablo.bernal.polo@gmail.com>
2026-06-07 22:25:04 +0200

    refactor: AssociatedLegendrePolynomialEvaluator to pre-compute
    the coefficients.

--- src/main/java/numericalLibrary/functions/AssociatedLegendrePolynomialEvaluator.java
+++ src/main/java/numericalLibrary/functions/AssociatedLegendrePolynomialEvaluator.java
@@ -4,6 +4,10 @@ package numericalLibrary.functions;
 
 public class AssociatedLegendrePolynomialEvaluator
 {
+	
+	private final double[][] alm;
+	private final double[][] blm;
+	
 	/**
 	 * Result of evaluation of P_l^m(x).
 	 */
@@ -18,10 +22,21 @@ public class AssociatedLegendrePolynomialEvaluator
 		if( lMaximum < 2 ) {
 			lMaximum = 2;
 		}
+		this.alm = new double[ lMaximum + 1 ][];
+		this.blm = new double[ lMaximum + 1 ][];
 		this.plm = new double[ lMaximum + 1 ][];
 		for( int l=0; l<=lMaximum; l++ ) {
 			// For each l, m ranges from -l to l.
 			this.plm[l] = new double[ l+1 ];
+			this.alm[l] = new double[ l+1 ];
+			this.blm[l] = new double[ l+1 ];
+			double lPlus1 = l + 1.0;
+			double lPluslPlus1 = l + lPlus1;
+			for( int m=0; m<=l; m++ ) {
+				double lMinus_mPlus1 = lPlus1 - m;
+				this.alm[l][m] = lPluslPlus1 / lMinus_mPlus1;
+				this.blm[l][m] = ( l + m ) / lMinus_mPlus1;
+			}
 		}
 		this.plm[0][0] = 1.0;
 	}
@@ -35,7 +50,7 @@ public class AssociatedLegendrePolynomialEvaluator
 		// ( l - m + 1 ) P_{l+1}^m(x) = ( 2 l + 1 ) x P_l^m(x) - (l+m) P_{l-1}^m(x)
 		for(  int l=1, lPlus1=2;  lPlus1<this.plm.length;  l++, lPlus1++  ) {
 			for( int m=0; m<l; m++ ) {
-				this.plm[lPlus1][m] = ( ( l + l + 1 ) * x * this.plm[l][m] - ( l + m ) * this.plm[l-1][m] ) / ( l - m + 1 );
+				this.plm[lPlus1][m] = this.alm[l][m] * x * this.plm[l][m] - this.blm[l][m] * this.plm[l-1][m];
 			}
 			// P_{l+1}^l(x) = ( 2 l + 1 ) x P_l^l(x)
 			// P_{l+1}^{l+1}(x) = - ( 2 l + 1 ) sqrt( 1 - x^2 ) P_l^l(x)
